c59c021156e6c6df3d1b79bd28f8e85058775cc4
roy.crippen4 <roy.crippen4@archarithms.com>
2024-04-30 16:42:21 -0400

    chore: formatting

--- lua/package-info/helpers/clean_version.lua
+++ lua/package-info/helpers/clean_version.lua
@@ -2,9 +2,9 @@
 -- @param value: string - value from which to strip ^ and ~ from
 -- @return string?
 return function(value)
-    if value == nil then
-        return nil
-    end
+  if value == nil then
+    return nil
+  end
 
-    return value:gsub("%^", ""):gsub("~", "")
+  return value:gsub('%^', ''):gsub('~', '')
 end
